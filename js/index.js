c83b28f2d9fb7b63ae68883322fe3a3e4ba9f00e
Knut Jackowski <doxanthropos@googlemail.com>
2016-06-08 07:16:44 +0200

    Removing the old stuff

--- js/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('#contact-list > a').click(function(){
-    $('#contact-list > a').removeClass("active");
-    $(this).addClass("active");
-  });
-  $('body').scrollspy({target: ".navbar"});
-});
